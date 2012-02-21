be747ba468c10696d4eb48acbd031f33bbf4c8f4
Zachary “Gamer_Z.” Yaro <zmyaro@gmail.com>
2012-02-21 13:55:30 -0500

    Removed option for eighth in prefs

--- prefs.php
+++ prefs.php
@@ -14,7 +14,7 @@
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.general);"><img src="public/images/themes/default/modules/prefs/icon.png" alt="" /> General</a></li>
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.intraboxes);"><img src="blank.png" alt="" /> Intraboxes</a></li>
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.news);"><img src="public/images/themes/default/modules/news/icon.png" alt="" /> News</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.eighth);"><img src="public/images/themes/default/modules/eighth/icon.png" alt="" /> Eighth Period</a></li>
+<!--					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.eighth);"><img src="public/images/themes/default/modules/eighth/icon.png" alt="" /> Eighth Period</a></li> -->
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.profiles);"><img src="public/images/themes/default/modules/profiles/icon.png" alt="" /> Profiles</a></li>
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.groups);"><img src="public/images/themes/default/modules/groups/icon.png" alt="" /> Groups</a></li>
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.joke);"><img src="blank.png" alt="" /> Other</a></li>
