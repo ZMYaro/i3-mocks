1cc7fe0b826f411712af4bcecf388e793d01da34
Zachary “Gamer_Z.” Yaro <zmyaro@gmail.com>
2012-02-21 13:40:24 -0500

    Fixed icons in prefs and added blank placeholder icon

--- prefs.php
+++ prefs.php
@@ -11,13 +11,13 @@
 			<div class="paneHeader">Preferences</div>
 			<div class="paneContent">
 				<ul class="menu">
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.general);"><img src="module_icons/prefs.png" alt="" /> General</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.intraboxes);"><img src="module_icons/blank.png" alt="" /> Intraboxes</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.news);"><img src="module_icons/news.png" alt="" /> News</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.eighth);"><img src="module_icons/eighth.png" alt="" /> Eighth Period</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.profiles);"><img src="module_icons/profiles.png" alt="" /> Profiles</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.groups);"><img src="module_icons/groups.png" alt="" /> Groups</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.joke);"><img src="module_icons/blank.png" alt="" /> Other</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.general);"><img src="public/images/themes/default/modules/prefs/icon.png" alt="" /> General</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.intraboxes);"><img src="blank.png" alt="" /> Intraboxes</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.news);"><img src="public/images/themes/default/modules/news/icon.png" alt="" /> News</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.eighth);"><img src="public/images/themes/default/modules/eighth/icon.png" alt="" /> Eighth Period</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.profiles);"><img src="public/images/themes/default/modules/profiles/icon.png" alt="" /> Profiles</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.groups);"><img src="public/images/themes/default/modules/groups/icon.png" alt="" /> Groups</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.joke);"><img src="blank.png" alt="" /> Other</a></li>
 				</ul>
 			</div>
 		</div>
