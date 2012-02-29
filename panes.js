05f8c5715a2453c6a3d1349bec2a15871c751baa
Zachary “Gamer_Z.” Yaro <zmyaro@gmail.com>
2012-02-29 15:48:43 -0500

    Added function to load EJS template in main pane (not currently used)

--- panes.js
+++ panes.js
@@ -1,26 +1,34 @@
-/**  */
-
 /** The currently selected menu item (null if no sub-pane is open) */
-var selectedTab = null;
+var selectedItem = null;
 
 /**
  * Runs when a menu item that opens a sub-panel is clicked
  * @param {HTMLLIElement} item - The list item that contains the link that was clicked
  */
 function clickedTab(item) {
-	if(selectedTab != null && selectedTab != item) {
-		selectedTab.className = null;
+	if(selectedItem != null && selectedItem != item) {
+		selectedItem.className = null;
 	}
 	item.className = "selected";
-	selectedTab = item;
+	selectedItem = item;
 }
 
 /**
- * Opens a second pane containing the given HTML
- * @param {string} template - The URL of the template to be displayed in the panel
+ * Opens the given template in the main pane
+ * @param {string} template - The URL of the template to be displayed
+ */
+function openPane(template) {
+	closeSubPane();
+	var mainPane = document.getElementById("mainPane");
+	mainPane.innerHTML = new EJS({url:template}).render({});
+}
+
+/**
+ * Opens a second pane containing the given template
+ * @param {string} template - The URL of the template to be displayed in the pane
  */
 function openSubPane(template) {
-	var subPane = document.getElementById("subPane")
+	var subPane = document.getElementById("subPane");
 	if (subPane != null) {
 		document.body.removeChild(subPane);
 	}
@@ -37,6 +45,10 @@ function openSubPane(template) {
 	// TODO: Replace this with adding a CSS class
 	document.getElementById("mainPane").style.position = "fixed";
 }
+
+/**
+ * Closes a previously opened sub-pane or does nothing if no sub-pane is open
+ */
 function closeSubPane() {
 	var subPane = document.getElementById("subPane");
 	if (subPane != null) {
@@ -57,9 +69,9 @@ function closeSubPane() {
 		      subPane.style.animationName = "closePane";
 		
 		
-		if(selectedTab != null) {
-			selectedTab.className = null;
-			selectedTab = null;
+		if(selectedItem != null) {
+			selectedItem.className = null;
+			selectedItem = null;
 		}
 		// TODO: Replace this with adding a CSS class
 		document.getElementById("mainPane").style.position = "absolute";
