b92d5072a2fc2ba94dc61de0f914d21f7e31339c
Zachary “Gamr_Z.” Yaro <zmyaro@gmail.com>
2012-05-29 19:34:12 -0400

    Updated icons on the prefs page

--- prefs/index.php
+++ prefs/index.php
@@ -12,12 +12,12 @@
 			<div class="paneContent">
 				<ul class="menu">
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('core.ejs');"><img src="../public/images/themes/default/modules/prefs/icon.png" alt="" /> General</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('intraboxes.ejs');"><img src="../blank.png" alt="" /> Intraboxes</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('intraboxes.ejs');"><img src="../public/images/themes/default/modules/prefs/ibox_prefs.png" alt="" /> Intraboxes</a></li>
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('news.ejs');"><img src="../public/images/themes/default/modules/news/icon.png" alt="" /> News</a></li>
 <!--					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane(prefs.eighth);"><img src="../public/images/themes/default/modules/eighth/icon.png" alt="" /> Eighth Period</a></li> -->
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('profiles.ejs');"><img src="../public/images/themes/default/modules/profiles/icon.png" alt="" /> Profiles</a></li>
 					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('groups.ejs');"><img src="../public/images/themes/default/modules/groups/icon.png" alt="" /> Groups</a></li>
-					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('joke.ejs');"><img src="../blank.png" alt="" /> Other</a></li>
+					<li><a href="#" onclick="clickedTab(this.parentElement);openSubPane('joke.ejs');"><img src="../public/images/themes/default/modules/prefs/other_prefs.png" alt="" /> Other</a></li>
 				</ul>
 			</div>
 		</div>
